5346512a0e91161e72cfe8580d5c686c000194f8
Naoya Yamashita <conao3@gmail.com>
2017-08-18 14:53:42 +0900

    tmp

--- MathJaxOption.js
+++ MathJaxOption.js
@@ -26,11 +26,12 @@ MathJax.Hub.Config({
 
             /* automatic bracing */
             /*quantity: ['{\\left\\{ #1 \\right\\}', 1],*/
-            qty:      ["{\\left\\( #1 \\right\\)}", 1],
+            qty:      ["{\\left\\( #1 \\right\\)}", 1]
+            /*,
             pqty:     ["{\\left\\( #1 \\right\\)}", 1],
             bqty:     ["{\\left\\[ #1 \\right\\]}", 1],
             vqty:     ["{\\left\\| #1 \\right\\|}", 1],
-            Bqty:     ["{\\left\\{ #1 \\right\\}}", 1],
+            Bqty:     ["{\\left\\{ #1 \\right\\}}", 1],*/
 	    }
     }
 });
