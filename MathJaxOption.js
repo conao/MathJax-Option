6dd0eb1eaaee6e57237d5b1263862deb999d7f40
conao <conao3@gmail.com>
2017-08-18 11:32:42 +0900

    inline math settings

--- MathJaxOption.js
+++ MathJaxOption.js
@@ -3,9 +3,14 @@ MathJax.Ajax.config.path["Extra"] = "http://conao3.com/mathjax/extensions/legacy
 MathJax.Hub.Config({
     "HTML-CSS":{
 	scale:120
-    }
+    },
+    tex2jax:{
+	inlineMath: [['$' , '$'], ['\\(' , '\\)']],
+	displayMath: [['$$' , '$$'], ['\[' , '\]']],
+	processEscapes: true
+    },
     TeX:{
-	extensions: ["[Extra]/physics/physics.js"],
+	extensions: ["[Extra]/physics/unpacked/physics.js"],
 	Macros: {
 	    C: '{\\mathbb C}',
 	    R: '{\\mathbb R}'
