92e84eefb5c766d4333859d754e91ac3343caed3
Naoya Yamashita <conao3@gmail.com>
2017-08-18 13:45:55 +0900

    add gitignore

--- MathJaxOption.js
+++ MathJaxOption.js
@@ -2,24 +2,24 @@ MathJax.Ajax.config.path["Extra"] = "http://conao3.com/mathjax/extensions/legacy
 
 MathJax.Hub.Config({
     "HTML-CSS":{
-	scale:120
+	    scale:120
     },
     tex2jax:{
-	inlineMath: [['$latex' , '$'], ['\\(' , '\\)']],
-	displayMath: [['$$' , '$$'], ['\[' , '\]']],
-	processEscapes: true
+	    inlineMath: [['$latex' , '$'], ['\\(' , '\\)']],
+	    displayMath: [['$$' , '$$'], ['\[' , '\]']],
+	    processEscapes: true
     },
     TeX:{
-	extensions: ["[Extra]/physics/unpacked/physics.js"],
-	Macros: {
-	    N: '{\\mathbb N}',
-	    Z: '{\\mathbb Z}',
-	    Q: '{\\mathbb Q}',
-	    R: '{\\mathbb R}',
-	    C: '{\\mathbb C}',
-	    grad: '{\\nabla\\,}',
-	    div:  '{\\nabla\\cdot\\,}',
-	    rot:  '{\\nabla\\times\\,}'
-	}
+	    extensions: ["[Extra]/physics/unpacked/physics.js"],
+	    Macros: {
+	        N: '{\\mathbb N}',
+	        Z: '{\\mathbb Z}',
+	        Q: '{\\mathbb Q}',
+	        R: '{\\mathbb R}',
+	        C: '{\\mathbb C}',
+	        grad: '{\\nabla\\,}',
+	        div:  '{\\nabla\\cdot\\,}',
+	        rot:  '{\\nabla\\times\\,}'
+	    }
     }
 });
