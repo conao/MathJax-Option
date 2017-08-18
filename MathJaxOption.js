393aca67108bf85500b53a98d6abfb4d8a176281
conao <conao3@gmail.com>
2017-08-18 09:07:52 +0900

    physics.js support

--- MathJaxOption.js
+++ MathJaxOption.js
@@ -1,5 +1,8 @@
+MathJax.Ajax.config.path["Extra"] = "http://conao3.com/mathjax/extensions/legacy";
+
 MathJax.Hub.Config({
     TeX:{
+	extensions: ["[Extra]/physics/physics.js"],
 	Macros: {
 	    C: '{\\mathbb C}',
 	    R: '{\\mathbb R}'
