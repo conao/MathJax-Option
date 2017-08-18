3e8519f7a9eff091a64e577618e1aa590036a857
Naoya Yamashita <conao3@gmail.com>
2017-08-18 14:25:34 +0900

    tmp

--- MathJaxOption.js
+++ MathJaxOption.js
@@ -25,8 +25,8 @@ MathJax.Hub.Config({
 	        rot:  ['{\\nabla\\times\\,}'],
 
             /* automatic bracing */
-            /*quantity: ['{\\left\\{ #1 \\right\\}', 1],
-            qty:      ["{\\left\\{ #1 \\right\\}", 1]*/
+            /*quantity: ['{\\left\\{ #1 \\right\\}', 1],*/
+            qty:      ["{\\left\\{ #1 \\right\\}}", 1]
 	    }
     }
 });
