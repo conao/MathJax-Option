1c2df085a87e86746d169281bb649b16643a5833
Naoya Yamashita <conao3@gmail.com>
2017-08-27 23:43:54 +0900

    enable extentions: AMScd.js

--- MathJaxOption.js
+++ MathJaxOption.js
@@ -10,7 +10,7 @@ MathJax.Hub.Config({
 	    processEscapes: true
     },
     TeX:{
-        extensions: {["color.js"], ["enclose.js"]},
+        extensions: ["color.js", "AMScd.js", "enclose.js"],
 	    Macros: {
             /* classies */
             N:                    ['{\\mathbb N}'],
